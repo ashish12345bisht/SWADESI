a4374df1938978712e6c8071b4e690058ece4649
Prince Kumar <princekumar35071@gmail.com>
2022-04-12 12:50:56 +0530

    add myaddress page with footer and some changes with oter folder

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import Home from './pages/Home';
 import Cart from './components/Cart';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Footer2 from './components/Footer2';
+import MyAddress from './pages/MyAddress';
 
 
 function App() {
   return (
     <div className="App">
-    <Header/>
+    {/* <Header/>
       <Cart />
       <Home />
-      <Footer/>
+      <Footer/> */}
+      {/* <Footer2/> */}
+      <MyAddress/>
     </div>
   );
 }
